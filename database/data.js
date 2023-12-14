301d4f20fa0387e3d0ae6169a63b9bb2637b43fc
Saif <droconos99@gmail.com>
2023-12-14 16:45:52 +0600

    finalised

--- database/data.js
+++ database/data.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-   host: "bouduqkljsqzlrgmtj5h-mysql.services.clever-cloud.com",
-   user: "udcssrf6cbags5xq",
-   password: "4kDf9W4cxD257M8IENV3",
-   database: "bouduqkljsqzlrgmtj5h",
-   waitForConnections: true,
-   connectionLimit: 10,
-   queueLimit: 0,
+   host: "localhost",
+   database: "bookpanda",
+   user: "root",
+   password: "CSE123",
+   // host: "bouduqkljsqzlrgmtj5h-mysql.services.clever-cloud.com",
+   // user: "udcssrf6cbags5xq",
+   // password: "4kDf9W4cxD257M8IENV3",
+   // database: "bouduqkljsqzlrgmtj5h",
+   // waitForConnections: true,
+   // connectionLimit: 10,
+   // queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
